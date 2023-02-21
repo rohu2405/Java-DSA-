87577cfd6888d5c9f515bbee17533ff4aecb911c
Rohit Patel <90998294+rohu2405@users.noreply.github.com>
2023-02-21 15:47:19 +0530

    next commit

---
+++ src/com/java/basics1/Practise_Problems.java
@@ -0,0 +1,130 @@
+package com.java.basics1;
+
+public class Practise_Problems {
+    public static void main(String[] args) {
+        // Question 1
+        /*
+        Write a java program that takes two number as input and display the product of two numbers?
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        // taking num1 as input through user
+        int num1 = input.nextInt();
+        System.out.println("Enter number 2");
+        // taking num2 as input through user
+        int num2 = input.nextInt();
+        // product of two number store in result variable
+        int result = num1*num2;
+        System.out.println(result);
+
+
+        // Question 2
+         Write a java program to print area and perimeter of circle?
+        int radius = 20;
+        double perimeter = 2*Math.PI*radius;
+        double area;
+        area = Math.PI*radius*radius;
+        System.out.println("Perimeter is : " + perimeter + "Area is : " + area);
+
+        // Question 3
+         Write a java program that read numbers in inches and converts into meters?
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number in inches");
+        double numInches = input.nextDouble();
+        double numMeters = numInches*0.0254;
+        System.out.println("Number in meter is : " + numMeters);
+
+        // Question 4
+         Take 3 numbers from the user and print the greatest number?
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        // taking num1 as input through user
+        int num1 = input.nextInt();
+        System.out.println("Enter number 2");
+        // taking num2 as input through user
+        int num2 = input.nextInt();
+        System.out.println("Enter number 3");
+        // taking num3 as input through user
+        int num3 = input.nextInt();
+
+        if(num1 > num2 && num1 > num3){
+            System.out.println(num1 + " is greatest number among all");
+        } else if(num2>num1 && num2>num3){
+            System.out.println(num2 + " is greatest number among all");
+        } else{
+            System.out.println(num3 + " is greatest number among all");
+        }
+
+        // Question 5
+         Write a java program that takes year from user and print whether that year us
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Year");
+        // taking year as input through user
+        int year = input.nextInt();
+
+        if(year%4==0){
+            System.out.println("Leap Year");
+        } else{
+            System.out.println("Not a Leap Year");
+        }
+
+        // Question 6
+         Write java program to swap two number?
+
+        int num1 = 12;
+        int num2 = 21;
+
+        int temp = 0;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println(num1 + " " + num2);
+
+        // Alternate Approach of number swapping (bit manipulation approach)
+        int num1 = 10;
+        int num2 = 20;
+
+        num1 = num1^num2;
+        num2 = num1^num2;
+        num1 = num1^num2;
+        System.out.println(num1 + " " + num2);
+
+        // Question 7
+         check number is even or odd
+        int num = 11;
+        if(num%2 == 0) {
+            System.out.println("Even");
+        } else{
+            System.out.println("Odd");
+        }
+
+        // Alternate approach (bit manipulation)
+        int num = 12;
+        if((num&1) == 0) {
+            System.out.println("Even");
+        }
+        if((num&1) == 1){
+            System.out.println("Odd");
+        }
+
+        // Question 8
+        // Write a java program to grade students based on their marks?
+        int marks = 70;
+        if(marks>90){
+            System.out.println("A+");
+        } else if(marks>80 && marks<=90){
+            System.out.println("A");
+        }else if(marks>70 && marks<=80){
+            System.out.println("B+");
+        }else if(marks>60 && marks<=70){
+            System.out.println("B");
+        } else{
+            System.out.println("Fail");
+       }*/
+
+
+
+
+
+
+    }
+}
